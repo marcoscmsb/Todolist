f8cc6dc64da8b928c247e980737dc98fcfa3257c
Marcos <marcoscmsb@outlook.com>
2021-10-02 17:48:12 -0400

    Primeiro commit iniciando projeto com vite

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <h1>{{ titulo }}</h1>
+  <br />
+  <label for="descricao">Descrição:</label>
+  <input
+    v-model="tarefa.descricao"
+    id="descricao"
+    type="text"
+    placeholder="Digite a descrição da tarefa"
+  />
+  <br />
+  <label for="data">Data:</label>
+  <input v-model="tarefa.data" id="data" type="date" />
+  <br />
+  <button @click="salvar">Salvar</button>
+  <br />
+
+  <TabelaTarefas :tarefas="tarefas"></TabelaTarefas>
+  
+</template>
+
+<script>
+import TabelaTarefas from './components/TabelaTarefas.vue'
+export default {
+  components: {
+    TabelaTarefas
+  },
+  data() {
+    return {
+      titulo: "Todolist Vite",
+      tarefa: {},
+      tarefas: [],
+    };
+  },
+  methods: {
+    salvar() {
+      this.tarefas.push(Object.assign({}, this.tarefa)); // deep copy
+    },
+  },
+};
+</script>
+
+<style>
+
+</style>
---
+++ src/components/TabelaTarefas.vue
@@ -0,0 +1,42 @@
+<template>
+  <table border="1">
+    <thead>
+      <th>Descrição</th>
+      <th>Data</th>
+      <th>Concluida</th>
+    </thead>
+    <tbody>
+      <tr
+        v-for="item in tarefas"
+        :key="item.descricao"
+        :class="{ riscado: item.concluida }"
+      >
+        <td>{{ item.descricao }}</td>
+        <td>{{ formatarData(item.data) }}</td>
+        <td>
+          <input v-model="item.concluida" type="checkbox" />
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+import moment from 'moment'
+export default {
+    props:["tarefas"],
+    methods: {
+        formatarData(data) {
+            return moment(data).format("DD/MM/yyyy")
+        }
+    }
+
+}
+</script>
+
+<style>
+.riscado {
+  text-decoration: line-through crimson;
+  
+}
+</style>
