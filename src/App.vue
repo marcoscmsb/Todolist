94aba946d784af7ed6c6f011d51cb6fec6c98265
Marcos <marcoscmsb@outlook.com>
2021-10-09 16:51:30 -0400

    Adicionando tailwindcss

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,66 @@
 <template>
-  <h1>{{ titulo }}</h1>
-  <br />
-  <label for="descricao">Descrição:</label>
-  <input
-    v-model="tarefa.descricao"
-    id="descricao"
-    type="text"
-    placeholder="Digite a descrição da tarefa"
-  />
-  <br />
-  <label for="data">Data:</label>
-  <input v-model="tarefa.data" id="data" type="date" />
-  <br />
-  <button @click="salvar">Salvar</button>
-  <br />
+  <h1 class="text-6xl text-center mt-28">{{ titulo }}</h1>
+  <div
+    class="
+      space-y-4
+      border-2 border-gray-300
+      w-1/3
+      rounded-sm
+      p-8
+      mx-auto
+      mt-20
+      
+    "
+  >
+    <div>
+      <label for="descricao" class="bolck">Descrição:</label>
+      <input
+        v-model="tarefa.descricao"
+        id="descricao"
+        type="text"
+        placeholder="Digite a descrição da tarefa"
+        class="
+          border-2 border-gray-300
+          outline-none
+          rounded-sm
+          px-4
+          py-1
+          focus:border-blue-600
+          w-full
+        "
+      />
+    </div>
+    <div>
+      <label for="data" class="block">Data:</label>
+      <input
+        v-model="tarefa.data"
+        id="data"
+        type="date"
+        class="
+          border-2 border-gray-300
+          outline-none
+          rounded-sm
+          px-4
+          py-1
+          focus:border-blue-600
+          w-full
+        "
+      />
+    </div>
+
+    <app-btn class="mx-auto" @click="salvar">Salvar</app-btn>
+  </div>
 
   <TabelaTarefas :tarefas="tarefas"></TabelaTarefas>
-  
 </template>
 
 <script>
-import TabelaTarefas from './components/TabelaTarefas.vue'
+import TabelaTarefas from "./components/TabelaTarefas.vue";
+import AppBtn from "./components/AppBtn.vue";
 export default {
   components: {
-    TabelaTarefas
+    TabelaTarefas,
+    AppBtn,
   },
   data() {
     return {
@@ -40,6 +77,4 @@ export default {
 };
 </script>
 
-<style>
-
-</style>
+<style></style>
---
+++ src/components/AppBtn.vue
@@ -0,0 +1,30 @@
+<template>
+  <button
+    type="button"
+    class="
+      px-4
+      py-3
+      bg-blue-600
+      rounded-md
+      text-white
+      outline-none
+      focus:ring-4
+      shadow-lg
+      transform
+      active:scale-x-75
+      transition-transform
+      mx-5
+      flex
+    "
+  >
+    <span>
+      <slot></slot>
+    </span>
+  </button>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
